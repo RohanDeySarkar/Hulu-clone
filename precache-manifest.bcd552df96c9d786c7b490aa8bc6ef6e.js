cadd920f63081b90104602d2a695b9fe7d04ace1
RohanDeySarkar <41055662+RohanDeySarkar@users.noreply.github.com>
2020-10-19 16:28:20 +0530

    Updates

---
+++ precache-manifest.bcd552df96c9d786c7b490aa8bc6ef6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739c0c9639adf4b3b909c36fb6b6e538",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8279726b93fae13bce31",
+    "url": "/static/css/main.c82eaaf3.chunk.css"
+  },
+  {
+    "revision": "30914d692b217a08f74a",
+    "url": "/static/js/2.2482274a.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.2482274a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8279726b93fae13bce31",
+    "url": "/static/js/main.d8135417.chunk.js"
+  },
+  {
+    "revision": "ac467fc18b9e2fff7bf5",
+    "url": "/static/js/runtime-main.186f3c4e.js"
+  }
+]);
